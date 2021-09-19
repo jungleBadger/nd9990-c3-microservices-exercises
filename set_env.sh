2f71a2911d55696e19cca558fc143993f4398e77
Daniel Abrao <danielpaladar@gmail.com>
2021-09-19 09:38:41 -0300

    Add missing files

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udacity
+export POSTGRES_PASSWORD=udacity321
+export POSTGRES_HOST=udagram-refactor-db.clxdmga8eqdq.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udagram-refactor-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=cloudeng
+export JWT_SECRET=testing
+export URL=http://localhost:8080
